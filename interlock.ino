03f292f3d8e2e5ff7c3d1de07adde80f03c3fd36
Gage Glupker <gglupker@umich.edu>
2016-05-20 16:53:28 -0400

    Basic framework

--- interlock.ino
+++ interlock.ino
@@ -1,12 +1,44 @@
-int pressureSensor = 0;
-int relayTrigger = 8;
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(6, 7); //Declare pins 6 and 7 as RX/TX
+
+int pressureSensor = 0; //Declare pin A0 to pressure sensor
+int relayTrigger = 8; //Declare pin 8 to control particle gun relay
+
+int pressureThreshold = 389; //floor(3.8/10 * 1024) for voltage reading to 10 bit
+int pressure = 0;
+int warningLED = 13;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(pressureSensor, INPUT);
+  digitalWrite(relayTrigger, LOW);
+  digitalWrite(warningLED, LOW);
+  
+  mySerial.begin(9600);
+  while(!mySerial){
+    ;
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  pressure = analogRead(pressureSensor);
 
+  if(pressure >= pressureThreshold){
+    stopProcess();
+    }
+    warning();
+    }
+  }
+  delay(10);
+}
+
+void stopProcess(){
+  if(mySerial.available()){
+    mySerial.println(3);
+  }
+  digitalWrite(relayTrigger, HIGH);
+}
+
+void warning(){
+  digitalWrite(warningLED, HIGH);
 }
